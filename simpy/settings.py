979d9f1d282dad64e20030b393ca0e2fdd17a4f8
omarElspaa <elspaa.omar@gmail.com>
2024-05-30 15:48:05 +0200

    follow the previous

--- simpy/settings.py
+++ simpy/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&k(zqd34q(_g&9z^md%rjl!&1-m%fg@co6ubf!zc+#ydft8w5g'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
