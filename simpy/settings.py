9122aad25b8442623ae3f22f6be77250520c8c9e
omarElspaa <elspaa.omar@gmail.com>
2024-05-30 15:47:00 +0200

    followed this video https://www.youtube.com/watch?v=wczWm8j4v9w

--- simpy/settings.py
+++ simpy/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&k(zqd34q(_g&9z^md%rjl!&1-m%fg@co6ubf!zc+#ydft8w5g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = []
 
